88dee10282ce163779e78d1d998e58c37e631402
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-05-14 09:03:22 -0300

    starting with frontend

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee8eb506d5ce50110bff39d3c2c613892d77d458")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620074eccffd35d8b14ed882ba37e0095b5eabcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navbar: "#1F2937",
+        whitesmoke: "whitesmoke",
+        button: "#367CB0",
+        buttonhover: "#6fb3f3",
+      },
+    },
+  },
+  plugins: [],
+};
