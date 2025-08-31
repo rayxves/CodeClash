46f5fdfcbec0cf2b039ac90d042da1b172c1fb7c
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-08-31 07:44:57 -0300

    update api timeout

--- frontend/api.ts
+++ frontend/api.ts
@@ -3,7 +3,7 @@ import type { CategoryViewData, CodeReference } from "./src/types/code";
 import Cookies from "js-cookie";
 const api = axios.create({
   baseURL: "https://codeclash-r6wh.onrender.com/api",
-  timeout: 70000,
+  timeout: 120000,
 });
 
 export const getCodeReferenceByFilters = async (
