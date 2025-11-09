b88a17c5a4ede0fa0d2da0b01d2c6ecd3d0bd092
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-11-09 06:08:01 -0300

    feat: add factory pattern to switch iterator

--- api/Composites/CodeComponent.cs
+++ api/Composites/CodeComponent.cs
@@ -4,11 +4,14 @@ namespace Composites;
 
 public abstract class CodeComponent : IAggregate<CodeComponent>
 {
+    private static readonly IIteratorFactory _iteratorFactory = new IteratorFactory();
+
     public int Id { get; protected set; }
     public string Name { get; protected set; } = string.Empty;
     public string Category { get; protected set; } = string.Empty;
     public string Language { get; protected set; } = string.Empty;
     public string Description { get; protected set; } = string.Empty;
+
     public abstract bool IsComposite();
     public virtual void Add(CodeComponent component) => throw new NotSupportedException();
     public abstract IEnumerable<CodeComponent> GetChildren();
@@ -16,13 +19,6 @@ public abstract class CodeComponent : IAggregate<CodeComponent>
 
     public IIterator<CodeComponent> CreateIterator(string mode)
     {
-        return mode.ToLower() switch
-        {
-            "breadth" => new BreadthFirstIterator(this),
-            "depth" => new DepthFirstIterator(this),
-            "algorithms" => new AlgorithmsOnlyIterator(this),
-            _ => new DepthFirstIterator(this)
-        };
+        return _iteratorFactory.CreateIterator(this, mode);
     }
-
-    }
+}
---
+++ api/Iterators/IIteratorFactory.cs
@@ -0,0 +1,22 @@
+using Composites;
+
+namespace Iterators;
+
+public interface IIteratorFactory
+{
+    IIterator<CodeComponent> CreateIterator(CodeComponent root, string mode);
+}
+
+public class IteratorFactory : IIteratorFactory
+{
+    public IIterator<CodeComponent> CreateIterator(CodeComponent root, string mode)
+    {
+        return mode.ToLower() switch
+        {
+            "breadth" => new BreadthFirstIterator(root),
+            "depth" => new DepthFirstIterator(root),
+            "algorithms" => new AlgorithmsOnlyIterator(root),
+            _ => new DepthFirstIterator(root)
+        };
+    }
+}
